6ca0146c3551f36d8f46f6ed40e320a3fbb324cb
Ann <annakarp363@gmail.com>
2019-02-10 22:06:20 +0200

    Updates

---
+++ precache-manifest.9b66d75619d1cdda5f593443af37793f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f86e540036323b92140483b835525a2",
+    "url": "/address-book-app/static/media/search.2f86e540.svg"
+  },
+  {
+    "revision": "96e00db6278d966a85e2",
+    "url": "/address-book-app/static/js/runtime~main.96e00db6.js"
+  },
+  {
+    "revision": "92ae6133c55effcd1468",
+    "url": "/address-book-app/static/js/main.92ae6133.chunk.js"
+  },
+  {
+    "revision": "a20bad00aa65520f9463",
+    "url": "/address-book-app/static/js/1.a20bad00.chunk.js"
+  },
+  {
+    "revision": "92ae6133c55effcd1468",
+    "url": "/address-book-app/static/css/main.bc14f37b.chunk.css"
+  },
+  {
+    "revision": "634dcb5152a10f917f58710787730874",
+    "url": "/address-book-app/index.html"
+  }
+];
